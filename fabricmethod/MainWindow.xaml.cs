9e6db5a8f01c47dc7ceee8c99e27b375f7f45896
grohotovde-svg <grohotovde@mer.ci.nsu.ru>
2026-03-19 11:48:10 +0700

    Update MainWindow.xaml.cs

--- fabricmethod/MainWindow.xaml.cs
+++ fabricmethod/MainWindow.xaml.cs
@@ -1,4 +1,4 @@
-﻿using System.Text;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -11,14 +11,72 @@ using System.Windows.Shapes;
 
 namespace fabricmethod
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
+    
     public partial class MainWindow : Window
     {
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        
+        private CircleCreator _circleCreator;
+        private SquareCreator _squareCreator;
+        private TriangleCreator _triangleCreator;
+        private RedFactory _redFactory;
+
+        private void ColorComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            
+            DrawingPanel.Children.Clear();
+
+            var selectedItem = (ComboBoxItem)ColorComboBox.SelectedItem;
+            string color = selectedItem.Content.ToString();
+
+            switch (color)
+            {
+                case "Красный":
+                    _circleCreator = new RedCircleCreator();
+                    _squareCreator = new RedSquareCreator();
+                    _triangleCreator = new RedTriangleCreator();
+                    break;
+                case "Синий":
+                    _circleCreator = new BlueCircleCreator();
+                    _squareCreator = new BlueSquareCreator();
+                    _triangleCreator = new BlueTriangleCreator();
+                    break;
+                case "Зелёный":
+                    _circleCreator = new GreenCircleCreator();
+                    _squareCreator = new GreenSquareCreator();
+                    _triangleCreator = new GreenTriangleCreator();
+                    break;
+                case "Красная фабрика":
+                    _redFactory = new RedFactory();
+                    _redFactory.CreateTriangle();
+                    break;
+            }
+        }
+
+        private void CreateButton_Click(object sender, RoutedEventArgs e)
+        {
+            
+        }
+
+        private void AddFigures_Click(object sender, RoutedEventArgs e)
+        {
+            if (_circleCreator == null) return;
+
+            
+            var circle = _circleCreator.CreateCircle();
+            var square = _squareCreator.CreateSquare();
+            var triangle = _triangleCreator.CreateTriangle();
+            
+
+            
+            DrawingPanel.Children.Add(circle.CreateUIElement());
+            DrawingPanel.Children.Add(square.CreateUIElement());
+            DrawingPanel.Children.Add(triangle.CreateUIElement());
+
         }
     }
 }
